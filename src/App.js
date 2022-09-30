c35a00bf5a9fda28df0012990d51fb7835ec8c7f
Saaim-K <saaimkhan221@gmail.com>
2022-10-01 01:41:47 +0500

    FunctionalComponents

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import FunctionalComponent from './Learning-React/FunctionalComponent';
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+      <FunctionalComponent />
+    </>
   )
 }
 
---
+++ src/Learning-React/FunctionalComponent.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+//-------------------- Normal Functional Component --------------------
+
+// export default function FunctionalComponent() {
+//   return (
+//     <>
+//       <h1>Functional Compnents</h1>
+//     </>
+//   )
+// }
+
+//-------------------- Normal Functional Component --------------------
+
+
+//-------------------- ES6 Functional Component --------------------
+
+// const FunctionalComponent = () => {
+//   return (
+//     <>
+//       <h1>Functional Components</h1>
+//     </>
+//   )
+// }
+
+// export default FunctionalComponent;
+
+
+//Since JSX is written in only one div therefore we can apply rules of one liner ES6 Arrow Functions
+
+// const FunctionalComponent = () =>
+//   <>
+//     <h1>Functional Components</h1>
+//   </>
+
+
+// export default FunctionalComponent;
+
+//-------------------- ES6 Functional Component --------------------
